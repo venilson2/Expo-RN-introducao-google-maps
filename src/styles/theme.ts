0ef4217feb209f4df31f0ea72b1c1ec9b7bcd46d
PhoenixBytee <venilson2009@hotmail.com>
2023-05-07 16:58:04 -0300

    refactor: ajuste na paleta de cores

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -20,19 +20,24 @@ export default {
 
     // Neutral Colors
     neutral_000: '#FFFFFF',
-    neutral_100: '#F5F8FA',
-    neutral_150: '#E4ECF0',
-    neutral_200: '#DDE9F0',
-    neutral_300: '#BDD2E8', //cor original
-    neutral_400: '#9DB8DF',
-    neutral_500: '#7C9EC6',
-    neutral_600: '#5C84AD',
+    neutral_100: '#F4F4F8',
+    neutral_150: '#F9F9F9',
+    neutral_200: '#E3E6E9',
+    neutral_350: '#BDD2E8', //cor original
+    neutral_300: '#CBD5E1',
+    neutral_400: '#94A3B8',
+    neutral_500: '#64748B',
+    neutral_600: '#505D6F',
+    neutral_700: '#3C4653',
+    neutral_800: '#282E38',
+    neutral_900: '#14171C',
+    neutral_alt: '#7C7C7C',
 
     // Status Colors
-    success: '#6ABD9B',
-    danger: '#BF3030',
-    warning: '#FFA07A',
-    info: '#4DA8DA,',
+    success: '#00C853',
+    danger: '#F44336',
+    warning: '#FFC107',
+    info: '#2196F3,',
   },
   fonts: {
     poppins: {
